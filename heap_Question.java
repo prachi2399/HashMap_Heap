fb5360499704c8586afda087ff7d6378452c3d2a
prachi2399 <prachi2399@gmail.com>
2021-05-22 00:48:02 +0530

    Update heap_Question.java

--- heap_Question.java
+++ heap_Question.java
@@ -37,6 +37,52 @@ public class heap_Question {
         return 1;
     }
 
+    // rabbit in forest
+    public int numRabbits(int[] answers) {
+        int count=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int ele:answers){
+            if(!map.containsKey(ele)){ 
+                map.put(ele,1);
+                count+=ele+1;
+            }
+            else map.put(ele,map.get(ele)+1);
+            if(map.get(ele)==ele+1) map.remove(ele);
+            
+        }
+        return count;
+    }
+
+    // gfg a+b=c+d
+    public int findPairs(int arr[],int n) 
+    {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int sum=arr[i]+arr[j];
+                map.put(sum,map.getOrDefault(sum,0)+1);
+                if(map.get(sum)==2) return 1;
+            }
+        }
+        return 0;
+    }
+
+    // incomplete Array
+    int countElements(int N, int A[]) {
+        // code here
+        if(N==1) return 0;
+        int min=(int)1e8;
+        int max=-(int)1e8;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int ele:A){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+            if(min>ele) min=ele;
+            if(max<ele) max=ele;
+        }
+        return (max-min-1)-(map.size()-2);
+    }
+
+    // kth largest in stream
     
 }
 
